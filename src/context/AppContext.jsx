7330ec170f4ec0bfa02bc454915419543478ea65
gauravdevkar27 <devkardinesh18@gmail.com>
2025-09-19 23:48:49 +0530

    sidebar components

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,19 +1,63 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { dummyChats, dummyUserData } from "../main_assets/assets";
+const AppContext = createContext(); 
+// global state container in react createContext() 
+// is a function from React that creates a Context object.
 
-const AppContext = createContext();
+//A Context is like a “global store” that lets you share 
+// values (state, functions, data) across your 
+//component tree without manually passing props at every level.
 
-export const AppContextProvider = ({children}) => {
- 
+export const AppContextProvider = ({ children }) => {
+
     const navigate = useNavigate();
+    const [user, setUser] = useState(null);
+    const [chats, setChats] = useState([]);
+    const [selectedChat, setSelectedChat] = useState(null);
+    const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
 
-    const value ={}
+    const fetchUser = async () => {
+        setUser(dummyUserData)
+    }
 
-   return(
-    <AppContext.Provider value={value}>
-        {children}
-    </AppContext.Provider>
-   )
+    const fetchUsersChats = () =>{
+        setChats(dummyChats)
+        setSelectedChat(dummyChats[0])
+    }
+
+    useEffect(() => {
+        if (theme === 'dark') {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+        }
+        localStorage.setItem('theme', theme);
+    }, [theme])
+
+    useEffect(()=>{
+        if(user){
+            fetchUsersChats()
+        }
+        else{
+            setChats([])
+            setSelectedChat(null)
+        }
+    }, [user])
+
+    useEffect(() => {
+        fetchUser()
+    },[])
+
+    const value = {navigate, user, setUser, fetchUser, chats, setChats, 
+        selectedChat, setSelectedChat, theme, setTheme
+    }
+
+    return (
+        <AppContext.Provider value={value}>
+            {children}
+        </AppContext.Provider>
+    )
 }
 
 export const useAppContext = () => useContext(AppContext)
