79d3023d8f2f06a96deaa5594125434c36737743
gauravdevkar27 <devkardinesh18@gmail.com>
2025-09-22 01:38:43 +0530

    Refactor chatbot component

--- src/component/ChatBox.jsx
+++ src/component/ChatBox.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useAppContext } from '../context/AppContext'
+import { assets } from '../main_assets/assets';
 
-function ChatBox() {
-  return (
-    <div>
-      ChatBox
-    </div>
-  )
+const ChatBox = () => {
+
+  const {selectedChat, theme} = useAppContext();
+  const [messages, setMessages] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if(selectedChat){
+      setMessages(selectedChat.messages)
+    }
+    
+  }, [selectedChat])
 }
+
+return (
+  <div>
+    <div className='flex-1 flex flex-col justify-between m-5 md:m-10 x1:mx-30
+    max-md:mt-14 2x1:pr-40'/> 
+
+    {/* Chat Messages */}
+    <div className='flex-1 mb-5 overflow-y-scroll'>
+       {messages.length === 0 &&
+       (
+        <div>
+          <img src={theme === 'dark' ? assets.logo_full : assets.logo_full_dark} alt=''
+          className='w-full max-w-56 sm:max-w-68'/>
+        </div>
+       ) 
+       } 
+    </div>
+
+    {/* Promt Input Box */}
+    <form>
+
+    </form>
+  </div>
+)
 
 export default ChatBox
