f55d213603a78b9ec864fa25198ce84dfc6ec6d4
gauravdevkar27 <devkardinesh18@gmail.com>
2025-09-23 00:07:45 +0530

    user messgae chat box

--- src/component/ChatBox.jsx
+++ src/component/ChatBox.jsx
@@ -14,30 +14,30 @@ const ChatBox = () => {
     }
     
   }, [selectedChat])
-}
 
-return (
-  <div>
-    <div className='flex-1 flex flex-col justify-between m-5 md:m-10 x1:mx-30
-    max-md:mt-14 2x1:pr-40'/> 
-
-    {/* Chat Messages */}
-    <div className='flex-1 mb-5 overflow-y-scroll'>
-       {messages.length === 0 &&
-       (
-        <div>
-          <img src={theme === 'dark' ? assets.logo_full : assets.logo_full_dark} alt=''
-          className='w-full max-w-56 sm:max-w-68'/>
-        </div>
-       ) 
-       } 
+  return (
+    <div className='flex-1 flex flex-col justify-between m-5 md:m-10 xl:mx-30 max-md:mt-14 2xl:pr-40'>
+      {/* Chat Messages */}
+      <div className='flex-1 mb-5 overflow-y-scroll'>
+        {messages.length === 0 &&
+        (
+          <div className="flex flex-col items-center justify-center h-full">
+            <img src={theme === 'dark' ? assets.logo_full : assets.logo_full_dark} alt=''
+            className='w-full max-w-56 sm:max-w-[17rem]'/>
+            <p className='mt-5 text-4xl sm:text-6xl text-center text-gray-400
+            dark:text-white'>Ask me anything</p>
+          </div>
+        ) 
+        } 
+        {messages.map((messages, index) => {})}
+      </div>
+  
+      {/* Promt Input Box */}
+      <form>
+  
+      </form>
     </div>
-
-    {/* Promt Input Box */}
-    <form>
-
-    </form>
-  </div>
-)
+  )
+}
 
 export default ChatBox
