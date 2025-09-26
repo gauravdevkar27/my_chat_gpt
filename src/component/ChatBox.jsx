6181eb24b6a4358984d90a17d08c559c64b588af
gauravdevkar27 <155799241+gauravdevkar27@users.noreply.github.com>
2025-09-26 23:48:44 +0530

    auto scrolling functionality

--- src/component/ChatBox.jsx
+++ src/component/ChatBox.jsx
@@ -5,6 +5,8 @@ import Message from './Message';
 
 const ChatBox = () => {
 
+  const containerRef = React.useRef(null);
+
   const { selectedChat, theme } = useAppContext();
   const [messages, setMessages] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -23,10 +25,19 @@ const ChatBox = () => {
 
   }, [selectedChat])
 
+  useEffect(() => {
+   if(containerRef.current){
+    containerRef.current.scrollTo({
+      top: containerRef.current.scrollHeight,
+      behavior: 'smooth',
+    })
+   }
+  },[messages])
+
   return (
     <div className='flex-1 flex flex-col justify-between m-5 md:m-10 xl:mx-30 max-md:mt-14 2xl:pr-40'>
       {/* Chat Messages */}
-      <div className='flex-1 mb-5 overflow-y-scroll'>
+      <div ref={containerRef} className='flex-1 mb-5 overflow-y-scroll'>
         {messages.length === 0 &&
           (
             <div className="flex flex-col items-center justify-center h-full">
